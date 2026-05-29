afb09f56ef655da6d710003d19b70cba92506448
stanqb <9857@holbertonstudents.com>
2026-05-29 11:12:52 +0200

    Fix: add heap_insert prototype to header

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -23,4 +23,7 @@ typedef struct binary_tree_s heap_t;
 /* Task 0 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
+/* Task 1 */
+heap_t *heap_insert(heap_t **root, int value);
+
 #endif /* BINARY_TREES_H */
